b97b321983078a8be9c5a161b1730e62bcf626df
Ben <benvmarden@gmail.com>
2020-02-20 19:27:20 -0800

    Add theme via appTheme function in theme/style.dart

---
+++ study_group_app/lib/theme/style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primaryColor: Color(0xff0DC5DA),
+    accentColor: Colors.grey[800],
+    secondaryHeaderColor: Color(0xff1b1b1b),
+    hintColor: Colors.white,
+    dividerColor: Colors.white,
+    buttonColor: Colors.grey[800],
+    scaffoldBackgroundColor: Colors.white,
+    canvasColor: Color(0xff6d6d6d),
+  );
+}
+
