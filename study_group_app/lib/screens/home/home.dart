8ddce5f88150ae8490cd23134ca3c14e6c025f45
Ben Marden <36089218+bmarden@users.noreply.github.com>
2020-02-29 21:58:36 -0800

    Separate drawer to it's own class (#20)
    Create a new file "drawer.dart" with a class that has the drawer in it.
    This can be used for navigation from anywhere within the app so having
    it in a separate file makes sense. All that needs to be done is call the
    function from a Scaffold drawer:

--- study_group_app/lib/screens/home/home.dart
+++ study_group_app/lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_group_app/screens/home/drawer.dart';
 
 // Stateful home page class.
 class HomePage extends StatefulWidget {
@@ -23,41 +24,7 @@ class _HomePageState extends State<HomePage> {
         ),
         // drawer sets the hamburger menu on the side that pops out
         // its' child is a ListView that holds all the elements held in the drawer
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            // Further expanding objects held in the drawer
-            children: <Widget>[
-              // Finally at setting first item, This is the big colored chunk
-              // with the header text in it in the upper half of drawer
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor, // from style.dart
-                ),
-                child: Text(
-                  'Study Groups',
-                  style: TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              // Now at the bottom half of the drawer
-              // ListTiles are fixed height rows that contain text and an icon
-              ListTile(
-                leading: Icon(Icons.group),
-                title: Text('Groups'),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-            ],
-          ),
-        ),
+        drawer: MainDrawer(),
 
         // Back on the home page. This is the button at the bottom of the page
         floatingActionButton: FloatingActionButton.extended(
