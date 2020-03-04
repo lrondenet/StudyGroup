382bac41049da6a3f0a56351125525bb728cd873
Ben <benvmarden@gmail.com>
2020-03-04 01:28:28 -0800

    Implement basic ListBuilder cards

--- study_group_app/lib/models/groups.dart
+++ study_group_app/lib/models/groups.dart
@@ -1,25 +1,13 @@
+import 'user.dart';
+
 class Group {
   int id;
   String name;
+  int members;
+  String meetDay;
+  String timeBlock;
 
-  Group(this.id, this.name);
-}
-
-class GroupProvider {
-  List<Group> _groups;
-  GroupProvider._() {
-    if (_groups == null) {
-      _groups.add(new Group(0, 'Fluffy Bunnies'));
-      _groups.add(new Group(1, 'Best Group'));
-      _groups.add(new Group(2, 'Other Best Group'));
-    }
-  }
-  // Singleton instance that will be used outside of this class
-  static final GroupProvider instance = GroupProvider._();
-
-  Future<List<Group>> getAllGroups() async {
-    return _groups;
-  }
+  Group(this.id, this.name, this.members, this.meetDay, this.timeBlock);
 }
 
 class UserStudyGroup {
@@ -29,6 +17,55 @@ class UserStudyGroup {
   UserStudyGroup(this.studyGrpId, this.userId);
 }
 
-class UserGroupsProvider {
-//  List<UserStudyGroup>
+class GroupProvider {
+  List<Group> _groups = new List<Group>();
+  List<UserStudyGroup> _userGroups = List<UserStudyGroup>();
+
+  GroupProvider._() {
+    if (_groups.isEmpty) {
+      _groups.add(new Group(0, 'Fluffy Bunnies', 4, 'Monday', '3-5pm'));
+      _groups.add(new Group(1, 'Best Group', 3, 'Wednesday', '2-3pm'));
+      _groups.add(new Group(2, 'Other Best Group', 2, 'Friday', '5-8pm'));
+    }
+    // UserStudyGroup(studyGrpId, userId)
+    if (_userGroups.isEmpty) {
+      _userGroups.add(new UserStudyGroup(0, 0));
+      _userGroups.add(new UserStudyGroup(0, 1));
+      _userGroups.add(new UserStudyGroup(0, 2));
+      _userGroups.add(new UserStudyGroup(0, 3));
+
+      _userGroups.add(new UserStudyGroup(1, 1));
+      _userGroups.add(new UserStudyGroup(1, 0));
+      _userGroups.add(new UserStudyGroup(1, 4));
+
+      _userGroups.add(new UserStudyGroup(2, 0));
+      _userGroups.add(new UserStudyGroup(2, 2));
+    }
+  }
+
+  // Singleton instance that will be used outside of this class
+  static final GroupProvider instance = GroupProvider._();
+
+  List<Group> getGroupsById(int userId) {
+    List<Group> grpList = new List<Group>();
+    List<UserStudyGroup> userGroups = getUserGroups(userId);
+    for (var userGrp in userGroups) {
+      for (var grp in _groups) {
+        if (userGrp.studyGrpId == grp.id) {
+          grpList.add(grp);
+        }
+      }
+    }
+    return grpList;
+  }
+
+  List<UserStudyGroup> getUserGroups(usrId) {
+    List<UserStudyGroup> userGroups = new List<UserStudyGroup>();
+    for (var userGrp in _userGroups) {
+      if (userGrp.userId == usrId) {
+        userGroups.add(userGrp);
+      }
+    }
+    return userGroups;
+  }
 }
--- study_group_app/lib/screens/home/group_card.dart
+++ study_group_app/lib/screens/home/group_card.dart
@@ -1,71 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:study_group_app/screens/home/drawer.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../models/user.dart';
+import '../../models/groups.dart';
 
-import 'group_card.dart';
-
-// Stateful home page class.
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-  final String title; // final keyword b/c title is in sub widget
+class GroupCards extends StatefulWidget {
+  GroupCards({Key key}) : super(key: key);
 
   @override
-// Creates the stateful widget HomePage
-  _HomePageState createState() => _HomePageState();
+  _GroupCards createState() => _GroupCards();
 }
 
-// Inherits from HomePage above
-class _HomePageState extends State<HomePage> {
+class _GroupCards extends State<GroupCards> {
+  final grps = GroupProvider.instance.getGroupsById(0);
+
   @override
-  // Main build function, generates the view
+  void initState() {
+    super.initState();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Scaffold is from MaterialApp. Implements the basic visual layout
-    return Scaffold(
-        // Top bar, title is set using widget.title which is passed down from main
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        // drawer sets the hamburger menu on the side that pops out
-        // its' child is a ListView that holds all the elements held in the drawer
-        drawer: MainDrawer(),
-        persistentFooterButtons: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed("/course-schedule");
-            },
-            color: Colors.blueAccent,
-            child: Text('Add Classes'),
-          )
-        ],
-        body: GroupCards(),
-
-        // Back on the home page. This is the button at the bottom of the page
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: null, // Doesn't actually do anything yet
-            label: Text('Find Group'),
-            icon: Icon(Icons.search)));
-  }
-
-  // Testing for Firebase iOS Configuration
-  int testNum = 0;
-  void getUpdate() {
-    Firestore.instance
-        .collection('test')
-        .document('lONyGDMN9NCvr9Vu4cxi')
-        .get()
-        .then((DocumentSnapshot ds) {
-      testNum = ds.data['count'];
-      print(testNum);
-      setState(() {});
-    });
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: grps.length,
+      itemBuilder: (BuildContext context, int index) {
+        Group cur = grps[index];
+        return Card(
+          child: Container(
+            child: ListTile(
+              title: Text(cur.name),
+              leading: Text(cur.meetDay.toString()),
+              trailing: Text(cur.timeBlock),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
-
-//class GroupCards extends StatelessWidget {
-//  GroupCards({Key key}) : super(key: key);
-//
-//  @override
-//  Widget build(BuildContext context) {
 //    return Column(
 //      children: <Widget>[
 //        Card(
