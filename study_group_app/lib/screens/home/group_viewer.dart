a70903dc0738224c75b883cfc9b31a5591b68655
Ben <benvmarden@gmail.com>
2020-03-08 13:24:28 -0700

    Restructure GroupView card widgets to separate functionality

--- study_group_app/lib/screens/home/group_viewer.dart
+++ study_group_app/lib/screens/home/group_viewer.dart
@@ -9,59 +9,54 @@ class GroupView extends StatefulWidget {
 }
 
 class _GroupState extends State<GroupView> {
+  // List of groups to be populated on initState
   List groups;
 
   @override
   void initState() {
+    // Get the current user's groups, currently hardcoded to be 2
     groups = GroupProvider.instance.getGroupsById(2);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    Material groupCardContent(Group group) => Material(
-          borderRadius: BorderRadius.circular(8.0),
-          color: Theme.of(context).accentColor,
-          child: InkWell(
-            customBorder: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            onTap: () {},
-            child: Container(
-              margin: EdgeInsets.fromLTRB(50.0, 16.0, 16.0, 20.0),
-//          constraints: BoxConstraints.expand(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    // Main content of the card, Sets placement and content of inner objects
+    Container groupCardContent(Group group) => Container(
+          margin: EdgeInsets.fromLTRB(50.0, 16.0, 16.0, 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(height: 5.0),
+
+              // Following Widgets are contents of each user's group.
+              // TODO Style and design
+              Text(group.name, style: Fonts.headerTextStyle),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 8.0),
+                  height: 1.5,
+                  width: 40.0,
+                  color: Colors.grey),
+              Row(
+                children: <Widget>[Expanded(child: Text(group.meetDay))],
+              ),
+              Row(
                 children: <Widget>[
-                  Container(height: 5.0),
-                  Text(group.name, style: Fonts.headerTextStyle),
-                  Container(
-                      margin: EdgeInsets.symmetric(vertical: 8.0),
-                      height: 1.5,
-                      width: 40.0,
-                      color: Colors.grey),
-                  Row(
-                    children: <Widget>[Expanded(child: Text(group.meetDay))],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Expanded(child: Text(group.timeBlock)),
-                      Expanded(
-                          child: Text(
-                              'Members: ${group.members}/${group.maxMembers}')),
-                    ],
-                  ),
+                  Expanded(child: Text(group.timeBlock)),
+                  Expanded(
+                      child: Text(
+                          'Members: ${group.members}/${group.maxMembers}')),
                 ],
               ),
-            ),
+            ],
           ),
         );
 
+    // Design of outer container, shape, height, on tap functions, ect
     Container groupCard(Group group) => Container(
           height: 120.0,
           margin: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
           decoration: BoxDecoration(
-//            shape: BoxShape.rectangle,
             borderRadius: BorderRadius.circular(15.0),
             boxShadow: <BoxShadow>[
               BoxShadow(
@@ -71,9 +66,21 @@ class _GroupState extends State<GroupView> {
               ),
             ],
           ),
-          child: groupCardContent(group),
+          // Wrap InkWell in Material Widget to show the splash effect on tap
+          child: Material(
+            borderRadius: BorderRadius.circular(8.0),
+            color: Theme.of(context).accentColor,
+            child: InkWell(
+              customBorder: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              onTap: () {},
+              child: groupCardContent(group),
+            ),
+          ),
         );
 
+    // Handles the scrolling behavior and bundled up Widgets to return to caller
     return Container(
       child: ListView.separated(
         scrollDirection: Axis.vertical,
@@ -83,6 +90,8 @@ class _GroupState extends State<GroupView> {
         itemBuilder: (BuildContext context, int index) {
           return groupCard(groups[index]);
         },
+
+        // May be a better way to do this, but builds a dividing space between each card
         separatorBuilder: (BuildContext context, int index) {
           return Divider(
             color: Theme.of(context).dividerColor,
