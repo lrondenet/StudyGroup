48a3b21428740e55bf9b536eed49c09c9c2e90c9
lrondenet <lrondenet1@mail.csuchico.edu>
2020-03-17 17:46:13 -0700

    updating main.dart

--- study_group_app/lib/main.dart
+++ study_group_app/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:study_group_app/screens/wrapper.dart';
 import 'package:study_group_app/services/auth.dart';
 import 'package:study_group_app/theme/style.dart';
-import 'package:study_group_app/screens/student/course_schedule.dart';
 import 'package:study_group_app/screens/student/select_classes.dart';
+import 'package:study_group_app/screens/wrapper.dart';
+import 'package:provider/provider.dart';
+import 'package:study_group_app/models/user.dart';
 
 
 void main() => runApp(MyApp());
@@ -12,19 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Study Buddy',
-      // Importing from theme/style.dart
-      theme: appTheme(),
-      // Sets the home page by calling HomePage and passing in title set above
-      home: Wrapper(title: 'Study Buddy App'), // Placeholder title for now
-      //home: CourseSchedulePage(title: 'My Course Schedule'),
-      routes: <String, WidgetBuilder> {
-        "/select-classes": (BuildContext context) => CourseSchedulePage()}
+    return StreamProvider<User>.value(
+      value: Auth().getUser,
+      child: MaterialApp(
+          title: 'Study Buddy',
+          // Importing from theme/style.dart
+          theme: appTheme(),
+          // Sets the home page by calling HomePage and passing in title set above
+          //home: CourseSchedulePage(title: 'My Course Schedule'),
+          home: Wrapper(title: 'Study Buddy'),
+          routes: <String, WidgetBuilder>{
+            "/select-classes": (BuildContext context) => new CourseSchedulePage(),
+          }),
     );
   }
 }
-
-
 
 
